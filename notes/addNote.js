242360dd60fef4c33e9fc1dca1f78786d257a0d2
pansslava <VeryCrazySlava@gmail.com>
2017-11-21 20:33:01 +0300

    working on dynamic node adding

---
+++ notes/addNote.js
@@ -0,0 +1,11 @@
+const $addprior = document.getElementById("addprior");
+const $addcommon = document.getElementById("addcommon");
+
+
+$addprior.addEventListener('click', function() {
+    console.log(1);
+})
+
+$addcommon.addEventListener('click', function() {
+    console.log(2);
+})
